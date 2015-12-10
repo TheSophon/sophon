4f8e0dfa4424d4db075d5733a960840a5f6ddfae
brickgao <brickgao@gmail.com>
2015-12-10 22:20:40 +0800

    Add test case for init_db() and drop_db()

---
+++ tests/test_database.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+
+from unittest import TestCase
+
+from sqlalchemy import create_engine, inspect
+
+from sophon.config import SQLALCHEMY_DATABASE_URI
+from sophon.database import drop_db, init_db
+
+
+class TestDatabase(TestCase):
+
+    def setUp(self):
+        self.engine = create_engine(SQLALCHEMY_DATABASE_URI)
+        self.inspector = inspect(self.engine)
+
+    def test_init_db(self):
+        init_db()
+        table_names = self.inspector.get_table_names()
+        self.assertListEqual(["user_meta"], table_names)
+
+    def test_drop_db(self):
+        init_db()
+        drop_db()
+        table_names = self.inspector.get_table_names()
+        self.assertListEqual([], table_names)
