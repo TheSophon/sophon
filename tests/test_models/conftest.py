dc292ae5be4d64783d0fd5340b1f520262d69632
brickgao <brickgao@gmail.com>
2015-12-19 22:16:00 +0800

    finish tests for user_meta

---
+++ tests/test_models/conftest.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+
+from sqlalchemy.orm import scoped_session
+
+from sophon.database import db_session, engine, init_db
+
+
+def mysql_fixture(func):
+    """Fixture for SQLAlchemy Session
+    """
+    def fixed_func(self, *args, **kwargs):
+        init_db()
+        connection = engine.connect()
+        transaction = connection.begin()
+        session = scoped_session(db_session)
+
+        fixed_args = tuple(list(args) + [session])
+
+        try:
+            func(self, *fixed_args, **kwargs)
+        finally:
+            session.remove()
+            transaction.rollback()
+            connection.close()
+
+    return fixed_func
