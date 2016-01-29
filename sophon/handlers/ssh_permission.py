9dbd9656c1da9a9dad7880063d44031731894a40
brickgao <brickgao@gmail.com>
2016-01-29 20:49:28 +0800

    implement function to get SSH permission

--- sophon/handlers/ssh_permission.py
+++ sophon/handlers/ssh_permission.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python2
 # -*- coding: utf-8 -*-
 
+import subprocess
+
 from tornado.web import authenticated
 
 from sophon.database import session
 from sophon.handlers import BaseHandler
-from sophon.models import UserMeta, SSHPermissionMeta
+from sophon.models import HostMeta, UserMeta, SSHPermissionMeta
 
 
 class SSHPermissionHandler(BaseHandler):
@@ -21,15 +23,24 @@ class SSHPermissionHandler(BaseHandler):
     @authenticated
     def patch(self):
         username = self.get_secure_cookie("username")
-        user_id = UserMeta.query.filter_by(username=username).first().id
+        user_item = UserMeta.query.filter_by(username=username).first()
+        user_id, public_key_path = user_item.id, user_item.public_key
         host_id, has_permission = (
             int(self.get_argument("host_id")),
             True if self.get_argument("has_permission") == "true" else False
         )
         if has_permission:
+            host_ip = HostMeta.get_all_hosts_status()[host_id]["IP"]
             ssh_permission_item = SSHPermissionMeta(
                 user_id=user_id, host_id=host_id
             )
             session.add(ssh_permission_item)
             session.commit()
+
+            print public_key_path, user_id
+
+            subprocess.call(
+                "cat " + public_key_path + " | ssh root@" + host_ip +
+                " \'cat >> ~/.ssh/authorized_keys\'", shell=True
+            )
         self.write({"msg": "success"})
