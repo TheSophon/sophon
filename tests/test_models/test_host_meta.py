ebcaeb43046452a9bb2d5017e3188ffe0e386b7e
brickgao <brickgao@gmail.com>
2016-02-01 19:49:57 +0800

    add test case for things related to dockers status on model layer

--- tests/test_models/test_host_meta.py
+++ tests/test_models/test_host_meta.py
@@ -34,7 +34,7 @@ class TestHostMeta(TestCase):
         ))
 
         self.assertEqual(_query_data.process_status, json.dumps([]))
-        self.assertEqual(_query_data.supervisor_status, json.dumps([]))
+        self.assertEqual(_query_data.dockers_status, json.dumps([]))
 
     @mysql_fixture
     def test_get_all_hosts_status(self, session):
@@ -46,7 +46,7 @@ class TestHostMeta(TestCase):
         ) as _query:
             hosts_status = HostMeta.get_all_hosts_status()
             self.assertEqual(hosts_status, {
-                1: {
+                hosts_status.keys()[0]: {
                     "Hostname": "Marvin",
                     "IP": "123.456.78.9",
                     "Status": "Down",
@@ -164,3 +164,64 @@ class TestHostMeta(TestCase):
                     "Command": "/bin/another/place/holder"
                 }]
             ))
+
+    @mysql_fixture
+    def test_get_all_hosts_dockers_status(self, session):
+        _dockers_status = [
+            {
+                "Container ID": "4c01db0b339c",
+                "Image": "ubuntu:12.04",
+                "Command": "bash",
+                "Created": "17 seconds ago",
+                "Status": "Up 16 seconds",
+                "Ports": "3300-3310/tcp",
+                "Names": "webapp"
+            }
+        ]
+        _insert_data = HostMeta(hostname="Marvin", ip="123.456.78.9")
+        _insert_data.dockers_status = json.dumps(_dockers_status)
+        session.add(_insert_data)
+
+        with mock.patch.object(
+            HostMeta, "query", session.query_property()
+        ) as _query:
+            dockers_status = HostMeta.get_all_hosts_dockers_status()
+            _result_dockers_status = _dockers_status[:]
+            _result_dockers_status[0].update({
+                "IP": "123.456.78.9",
+                "Hostname": "Marvin"
+            })
+            self.assertEqual(dockers_status, _result_dockers_status)
+
+    @mysql_fixture
+    @mock.patch("sophon.models.host_meta.session")
+    def test_update_hosts_dockers_status(self, session, _session):
+        _dockers_status = [
+            {
+                "Container ID": "4c01db0b339c",
+                "Image": "ubuntu:12.04",
+                "Command": "bash",
+                "Created": "17 seconds ago",
+                "Status": "Up 16 seconds",
+                "Ports": "3300-3310/tcp",
+                "Names": "webapp"
+            }
+        ]
+        _commit = mock.Mock()
+        _session.commit = _commit
+        _insert_data = HostMeta(hostname="Marvin", ip="123.456.78.9")
+        session.add(_insert_data)
+
+        with mock.patch.object(
+            HostMeta, "query", session.query_property()
+        ) as _query:
+            HostMeta.update_host_dockers_status(
+                ip="123.456.78.9", dockers_status=_dockers_status
+            )
+            _commit.called_once_with()
+            _query_data = session.query(HostMeta).filter_by(
+                hostname="Marvin"
+            ).first()
+            self.assertEqual(
+                _query_data.dockers_status, json.dumps(_dockers_status)
+            )
