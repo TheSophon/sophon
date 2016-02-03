554742bfe58a0d1d88d2b807defb754f68635e53
brickgao <brickgao@gmail.com>
2016-02-03 21:32:03 +0800

    add DeployHandler

---
+++ sophon/handlers/deploy.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+
+from tornado.web import authenticated
+
+from sophon.handlers import BaseHandler
+from sophon.models import DeployMeta
+from sophon.utils.time_convertor import created2str
+
+
+class DeployHandler(BaseHandler):
+
+    @authenticated
+    def get(self):
+        result = DeployMeta.get_all_deploy_summary()
+        for key in result.keys():
+            result[key]["Created"] = created2str(
+                result[key]["Created"]
+            )
+        self.write(result)
