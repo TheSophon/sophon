3dd6b265a25a736b7effbc39be684ffc1843915b
brickgao <brickgao@gmail.com>
2016-02-06 21:04:36 +0800

    add test case for model related to the detail of deployment

--- tests/test_models/test_deploy_meta.py
+++ tests/test_models/test_deploy_meta.py
@@ -79,3 +79,27 @@ class TestDeployMeta(TestCase):
             _id = summary.keys()[0]
             self.assertEqual(summary[_id]["Taskname"], "sample_task")
             self.assertEqual(summary[_id]["Status"], 0)
+
+    @mysql_fixture
+    def test_get_deploy_item_by_id(self, session):
+        _insert_data = DeployMeta(
+            taskname="sample_task",
+            user_id=42,
+            repo_uri="git@github.com:user/repo.git",
+            entry_point="sample",
+            hosts=[1, 2]
+        )
+        session.add(_insert_data)
+        _id = session.query(DeployMeta).all()[-1].id
+
+        with mock.patch.object(
+            DeployMeta, "query", session.query_property()
+        ) as _query:
+            deploy_item = DeployMeta.get_deploy_item_by_id(deploy_id=_id)
+            self.assertEqual(deploy_item["Taskname"], "sample_task")
+            self.assertEqual(deploy_item["Status"], 0)
+            self.assertEqual(deploy_item["Repo URI"],
+                             "git@github.com:user/repo.git")
+            self.assertEqual(deploy_item["Entry Point"], "sample")
+            self.assertEqual(deploy_item["Hosts"], [1, 2])
+            self.assertEqual(deploy_item["Msg"], "")
