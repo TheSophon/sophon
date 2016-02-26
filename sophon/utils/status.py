b8672323ef39f5281dcc442f3b1c59243a0c1b21
DevilBin <whu.evil.binary@gmail.com>
2016-02-26 23:11:48 +0800

    fix get_host_status

--- sophon/utils/status.py
+++ sophon/utils/status.py
@@ -1,50 +1,46 @@
 #!/usr/bin/env python2
 # -*- coding: utf-8 -*-
 
-import commands
+import subprocess
+
 
 command = {
     "cpu": "top -b -n 1",
     "disk": "df -h ."
 }
 
-def get_index(result, word):
-    index = 0
-    for x in result:
-        index = index + 1
-        if x == word:
-            return index
-    return 0
 
-def get_host_status():
+def get_host_status(ip):
     ret = {
-        "Status": "",
-        "CPU Load": "", 
-        "Memory Usage": "",
-        "Disk Usage": ""
+        "Status": "Down",
+        "CPU Load": 0,
+        "Memory Usage": (0, 0),
+        "Disk Usage": (0, 0)
     }
-    cpu = 'ansible all -a "%s"' % (command["cpu"])
-    disk = 'ansible all -a "%s"' % (command["disk"])
-    cpu_result = commands.getstatusoutput(cpu)
-    disk_result = commands.getstatusoutput(disk)
-    cpu_result = cpu_result[1].split()
-    disk_result = disk_result[1].split()
-    if cpu_result[2] == "FAILED":
-        ret["Status"] = "Down"
-        ret["CPU Load"] = 0
-        ret["Memory Usage"] = (0, 0)
-        ret["Disk Usage"] = (0, 0)
-    else:
-        cpu_index = get_index(cpu_result, "average:")
-        mem_index = get_index(cpu_result, "Mem:")
-        disk_index = get_index(disk_result, "/")
-        mem_used = int(cpu_result[mem_index + 2][:-1]) 
-        mem_not_used = int(cpu_result[mem_index][:-1]) - mem_used
-        disk_used = float(disk_result[disk_index - 4][:-1])
-        disk_not_used = float(disk_result[disk_index - 3][:-1])
+    cpu = "ansible {0} -a \"{1}\"".format(ip, command["cpu"])
+    disk = "ansible {0} -a \"{1}\"".format(ip, command["disk"])
+    _cpu_result = subprocess.Popen(
+        cpu, shell=True, stdout=subprocess.PIPE
+    ).stdout.read()
+    _disk_result = subprocess.Popen(
+        disk, shell=True, stdout=subprocess.PIPE
+    ).stdout.read()
+    cpu_result = _cpu_result.split()
+    disk_result = _disk_result.split()
+    if "top" in _cpu_result:
+        cpu_index = cpu_result.index("average:")
+        mem_index = cpu_result.index("Mem:")
+        disk_index = disk_result.index("/")
+        cpu_used = float(cpu_result[cpu_index + 1][:-1])
+        mem_used = int(cpu_result[mem_index + 3][:-1])
+        mem_not_used = int(cpu_result[mem_index + 1][:-1]) - mem_used
+        disk_used = float(disk_result[disk_index - 3][:-1])
+        disk_total = float(disk_result[disk_index - 4][:-1])
         if cpu_index:
-            ret["Status"] = "Active"
-            ret["CPU Load"] = cpu_result[cpu_index]
-            ret["Memory Usage"] = (mem_used / 1024, mem_not_used / 1024) 
-            ret["Disk Usage"] = (disk_used, disk_not_used)
-    print ret
+            ret = {
+                "Status": "Active",
+                "CPU Load": cpu_used,
+                "Memory Usage": (mem_used / 1024, mem_not_used / 1024),
+                "Disk Usage": (disk_used, disk_total)
+            }
+    return ret
