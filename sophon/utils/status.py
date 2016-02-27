57a124908e4d56e60ec75448c72f6f8c5203f141
brickgao <brickgao@gmail.com>
2016-02-27 17:15:39 +0800

    Fix get_host_status
    This change due to the different output of top between Ubuntu and Debian

--- sophon/utils/status.py
+++ sophon/utils/status.py
@@ -25,20 +25,20 @@ def get_host_status(ip):
     disk_result = subprocess.Popen(
         disk, shell=True, stdout=subprocess.PIPE
     ).stdout.read().split()
-    if "top" in "".join(cpu_result):
+    if "Cpu" in "".join(cpu_result):
         cpu_index = cpu_result.index("average:")
         mem_index = cpu_result.index("Mem:")
         disk_index = disk_result.index("/")
         cpu_used = float(cpu_result[cpu_index + 1][:-1])
-        mem_used = int(cpu_result[mem_index + 3][:-1])
-        mem_not_used = int(cpu_result[mem_index + 1][:-1]) - mem_used
+        mem_used = int(cpu_result[mem_index + 3].rstrip("K"))
+        mem_all = int(cpu_result[mem_index + 1].rstrip("K"))
         disk_used = float(disk_result[disk_index - 3][:-1])
         disk_total = float(disk_result[disk_index - 4][:-1])
         if cpu_index:
             ret = {
                 "Status": "Active",
                 "CPU Load": cpu_used,
-                "Memory Usage": (mem_used / 1024, mem_not_used / 1024),
+                "Memory Usage": (mem_used / 1024, mem_all / 1024),
                 "Disk Usage": (disk_used, disk_total)
             }
     return ret
